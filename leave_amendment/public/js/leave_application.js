d7570775e5f5db0e9dfe45bef63e0b478640246d
Craftint <shajeer@craftinteractive.ae>
2024-11-18 12:51:24 +0400

    dialouge field edit

--- leave_amendment/public/js/leave_application.js
+++ leave_amendment/public/js/leave_application.js
@@ -6,32 +6,20 @@ frappe.ui.form.on('Leave Application', {
 		            title : 'Amend application',
 		            fields:[
 		                {
-		                    label: 'From Date',
+		                    label: 'Early Joining Date',
                             fieldname: 'from_date',
                             fieldtype: 'Date',
-                            default:frm.doc.from_date
-		                },
-		                {
-		                    label: '',
-                            fieldname: 'col_brk',
-                            fieldtype: 'Column Break'
-		                },
-		                {
-		                    label: 'To Date',
-                            fieldname: 'to_date',
-                            fieldtype: 'Date',
-                            default:frm.doc.to_date,
-                            read_only:1
-		                },
+                            default:frm.doc.to_date
+		                }
     		          ],
     		          size: 'large',
     		          primary_action_label: 'Cancel Attendance',
     		          primary_action(values) {
 		          		if(values.from_date < frm.doc.from_date){
-                            	frappe.throw("From date can not be before leave application date")
+                            	frappe.throw("Early joining date can not be before leave application date")
                             }
-                        if(values.from_date > d.get_value('to_date')){
-                        	frappe.throw("From date can not be after to date")
+                        if(values.from_date > frm.doc.to_date){
+                        	frappe.throw("Early joining date can not be after to date")
                         }
 						frappe.call({
 							method:"leave_amendment.events.leave_amendment.amend_leaves",
